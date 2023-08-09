12fad9ef8c05b67da9e71d87ce1106c10b36ef54
EzequielMagalhaes <zekes_243@hotmail.com>
2023-08-09 15:51:28 -0300

    Salvando curso

--- src/application/MainProgram.java
+++ src/application/MainProgram.java
@@ -7,7 +7,7 @@ public class MainProgram {
 
 	public static void main(String[] args) {
 		Connection conn = DB.getConnetion(); // Abriu a conexão com o banco de dados.
-		System.out.println("Conexão cocluída!");
+		System.out.println("Conectado com sucesso!");
 		DB.closeConnection(); // Fechou a conexão com o banco de dados.
 	}
 }

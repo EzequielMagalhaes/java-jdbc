b4e0d39323440d4176e04fe526580b64e47f29b1
EzequielMagalhaes <zekes_243@hotmail.com>
2023-08-10 11:45:17 -0300

    Salvando curso

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,34 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import db.DB;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Connection conn = DB.getConnetion(); // Abriu a conexão com o banco de dados.
-		System.out.println("Conectado com sucesso!");
-		DB.closeConnection(); // Fechou a conexão com o banco de dados.
+//		Connection conn = DB.getConnetion(); // Abriu a conexão com o banco de dados.
+//		System.out.println("Conectado com sucesso!");
+//		DB.closeConnection(); // Fechou a conexão com o banco de dados.
+		
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DB.getConnetion();
+			st = conn.createStatement();
+			rs = st.executeQuery("select * from department;");
+			
+			while(rs.next()) {
+				System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+			}
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
 	}
 }
